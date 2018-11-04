1d7dde81d75ac4887c37ed6e46147830a6c49ca5
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-05 00:42:20 +0900

    Update join.php

--- join.php
+++ join.php
@@ -4,7 +4,6 @@ include "head.php";
 <form method=post action=join_ok.php>
 <table>
 <tr><td>username</td><td><input type=text name=username></td></tr>
-<tr><td>e-mail</td><td><input type=text name=email></td></tr>
 <tr><td>password</td><td><input type=password name=password1></td></tr>
 <tr><td>retype password</td><td><input type=password name=password2></td></tr>
 <tr><td colspan=2><input type=submit value=join></td></tr>
