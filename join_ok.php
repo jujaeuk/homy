0d91be74b0f5c192cd99ce6c735d803695fc935c
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-04 19:40:35 +0900

    Create join_ok.php

---
+++ join_ok.php
@@ -0,0 +1,20 @@
+<?
+include "db_access.php";
+include "head.php";
+$que="select * from users where username='".$_POST['username']."'";
+$result=mysqli_query($connect,$que);
+if(mysqli_num_rows($result)>0) $error_message="user name already exists";
+if($_POST['password1']="") $error_messsage="no password";
+if($_POST['password1']!=$_post['password2']) $error_message="different passwords";
+if($error_message){
+  echo "ERROR: ".$error_message."<br>\n";
+  echo "<a href=join.php>back</a>\n";
+}
+else{
+  $crypt_pass=crypt($_POST['password1'],"onlyone");
+  $que="insert into users (username, password) values('".$_POST['username']."','".$crypt_pass."')";
+  mysqli_query($connect,$que);
+  echo "user ".$_POST['username']." created<br><a href=.>home</a>\n";
+}
+?>
+</body></html>
