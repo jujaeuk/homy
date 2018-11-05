4683d3399bee03574a87d08f88f6a4fe9cc97506
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-06 00:12:13 +0900

    Update head.php

--- head.php
+++ head.php
@@ -1,8 +1,11 @@
 <!doctype html>
 <html>
 <head>
-<title></title>
+<title>Jaeuk Ju</title>
 <meta http-equiv="content-type" content="text/html;charset=utf-8">
 <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+  <header>
+    <h1>wonderful world, beautiful life</h1>
+  </header>
