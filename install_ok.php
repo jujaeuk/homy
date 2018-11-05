60718be32ae9f0c883a13372f32601f5257b949d
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-06 00:26:57 +0900

    Update install_ok.php

--- install_ok.php
+++ install_ok.php
@@ -4,16 +4,17 @@ if(mysqli_num_rows(mysqli_query($connect,"show tables like '".$_POST['homename']
   echo "ERROR: home name alreay exists<br><a href=install.php>back</a>\n";
 }
 else{
-  $fp=fopen("homename.txt","w");
+  mkdir("data");
+  $fp=fopen("data/homename.txt","w");
   fwrite($fp,$_POST['homename']);
   fclose($fp);
   
-  $fp=fopen("db_access.php","w");
+  $fp=fopen("data/db_access.php","w");
   fwrite($fp, "<?\n\$host=\"".$_POST['host']."\";\n\$user=\"".$_POST['user']."\";\n\$password=\"".$_POST['password'].
          "\";\n\$db=\"".$_POST['db']."\";\n");
   fwrite($fp, "\$connect=mysqli_connect(\$host,\$user,\$password,\$db) or die(\"DB connection error\");\n?>");
   fclose($fp);
-  include "db_access.php";
+  include "data/db_access.php";
   $que="create table ".$_POST['homename']."_users(
     no int not null auto_increment,
     unique(no),
