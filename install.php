77a11265a7b7dba4a9616ff0995a9fa4fc944e84
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-05 11:18:43 +0900

    Create install.php

---
+++ install.php
@@ -0,0 +1,9 @@
+<?
+include "head.php";
+?>
+<form method=post action=install_ok.php>
+<table>
+<tr><td>name</td><td><input type=text name=homename></td><td><input type=submit value=install></td></tr>
+</table>
+</form>
+</body></html>
