fc9ad30b1316d56cba8f92e1633d2954fb90516f
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-05 13:52:33 +0900

    Update install.php

--- install.php
+++ install.php
@@ -3,7 +3,13 @@ include "head.php";
 ?>
 <form method=post action=install_ok.php>
 <table>
-<tr><td>name</td><td><input type=text name=homename></td><td><input type=submit value=install></td></tr>
+  <tr><td>home name</td><td><input type=text name=homename></td></tr>
+  <tr><td colspan=2>mysql</td></tr>
+  <tr><td>host</td><td><input type=text name=host></td></tr>
+  <tr><td>user</td><td><input type=text name=user></td></tr>
+  <tr><td>password</td><td><input type=password name=password></td></tr>
+  <tr><td>db</td><td><input type=text name=db></td></tr>
+  <tr><td colspan=2><input type=submit value=install><td></tr>
 </table>
 </form>
 </body></html>
