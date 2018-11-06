39c16967ce2cf90ba7053ed9ad90735d88b05f71
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-06 16:53:52 +0900

    Update join_ok.php

--- join_ok.php
+++ join_ok.php
@@ -1,5 +1,5 @@
 <?
-include "db_access.php";
+include "data/db_access.php";
 include "lib.php";
 include "head.php";
 $que="select * from ".$homename."_users where name='".$_POST['username']."'";
@@ -7,6 +7,7 @@ $result=mysqli_query($connect,$que);
 if(mysqli_num_rows($result)>0) $error_message="user name already exists";
 if($_POST['password1']=="") $error_messsage="no password";
 if($_POST['password1']!=$_POST['password2']) $error_message="different passwords";
+echo "<article>\n";
 if($error_message){
   echo "ERROR: ".$error_message."<br>\n";
   echo "<a href=join.php>back</a>\n";
@@ -18,4 +19,6 @@ else{
   echo "user ".$_POST['username']." created<br><a href=.>home</a>\n";
 }
 ?>
+</article>
+</div></div>
 </body></html>
