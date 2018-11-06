2832e23474220a20330cb82950ae9fc31bfd47b5
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-06 10:27:40 +0900

    Update head.php

--- head.php
+++ head.php
@@ -9,8 +9,3 @@
   <header>
     <h1>wonderful world, beautiful life</h1>
   </header>
-<?
-  if(!file_exists("data")){
-    echo "<article>this home is not installed<br><a href=install.php>install</a></article>\n";
-  }
-?>
