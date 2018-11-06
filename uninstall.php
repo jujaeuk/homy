66a372c67c0e7a617776a083f29841deb41bee46
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-06 16:50:08 +0900

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,15 @@
 <?
 include "db_access.php";
 include "lib.php";
-
 $que="drop table ".$homename."_users";
 mysqli_query($connect,$que);
-unlink("homename.txt");
-unlink("db_access.php");
+$que="drop table ".$homename."_board";
+mysqli_query($connect,$que);
+unlink("data/homename.txt");
+unlink("data/db_access.php");
+rmdir("data");
 include "head.php";
-echo "$homename uninstalled\n";
+echo "<article>$homename uninstalled</article>\n";
 ?>
+</div></div>
 </body></html>
