40528ca01ed40456d0e896ef157aa079e75dfb3c
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-06 16:38:24 +0900

    Update base.php

--- base.php
+++ base.php
@@ -1,5 +1,10 @@
 <?
+include "lib.php";
+include "db_access.php";
 include "head.php";
+echo "<article>\n";
 echo "this is a homy base file.\n";
+echo "</article>\n";
 ?>
+</div></div>
 </body></html>
