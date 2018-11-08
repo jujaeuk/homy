e259c06567eae67dec57c2ad4b493b3bfb04a38d
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-08 11:21:17 +0900

    Update head.php

--- head.php
+++ head.php
@@ -5,7 +5,8 @@
 <meta http-equiv="content-type" content="text/html;charset=utf-8">
 <meta name="viewport" content="width=device-width, user-scalable=no">
 <?
-echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n";
+if(is_mobile()) echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n";
+else echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"stylem.css\">\n";
 ?>
 </head>
 <body>
