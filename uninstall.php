dcc8859d3a6fdee5a8f59d63112149d22242e87f
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-09 14:12:12 +0900

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,19 @@
 <?
 include "data/db_access.php";
 include "lib.php";
-$que="drop table ".$homename."_users";
-mysqli_query($connect,$que);
-$que="drop table ".$homename."_board";
-mysqli_query($connect,$que);
-unlink("data/homename.txt");
-unlink("data/db_access.php");
-rmdir("data");
 include "head.php";
-echo "<article>$homename uninstalled</article>\n";
+$que="select * from ".$homename."_usere where name='".$_COOKIE['user']."'";
+$check=mysqli_fetch_object(mysqli_query($connect,$que));
+if($check->no==1){
+  $que="drop table ".$homename."_users";
+  mysqli_query($connect,$que);
+  $que="drop table ".$homename."_board";
+  mysqli_query($connect,$que);
+  unlink("data/homename.txt");
+  unlink("data/db_access.php");
+  rmdir("data");
+  echo "<article>$homename uninstalled</article>\n";
+else echo "<article>you don't have the right</article>\n";
 ?>
 </div></div>
 </body></html>
