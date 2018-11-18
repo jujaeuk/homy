f3aafb57d654bd08b4ed078517d41f15cde70491
Jaeuk Ju <jujaeuk@gmail.com>
2018-11-18 18:31:18 +0900

    Update board_backup.php

--- board_backup.php
+++ board_backup.php
@@ -1,18 +1,17 @@
 <?
 include "lib.php";
 include "data/db_access.php";
-$now=time();
 $fp=fopen("data/board.txt","w");
 $que="select * from ".$homename."_board order by time";
 $result=mysqli_query($connect,$que);
 $i=0;
 while(@$check=mysqli_fetch_object($result)){
   fwrite($fp,"no. ".$check->no." ".$check->title." (".$check->writer.", ".date("Y-m-d H:i",$check->time)."\n");
-  fwrite($fp,$check->content."\n");
+  fwrite($fp,$check->content.")\n");
   if($check->upper!=0){
     $que="select * from ".$homename."_board where no=$check->upper";
     $check_upper=mysqli_fetch_object(mysqli_query($connect,$que));
-    fwrite($fp,"upper: no.".$check_uper->no." ".$check_upper->title\n");
+    fwrite($fp,"upper: no.".$check_upper->no." ".$check_upper->title."\n");
   }
   $que="select * from ".$homename."_board where upper=".$check->no;
   $result_lower=mysqli_query($connect,$que);
