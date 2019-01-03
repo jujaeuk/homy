693d5454c17d2aef921de68d16d0460cb7f4ea9f
Jaeuk Ju <jujaeuk@gmail.com>
2019-01-03 10:49:07 +0900

    Update write_ok.php

--- write_ok.php
+++ write_ok.php
@@ -1,7 +1,7 @@
 <?
 include "lib.php";
 include "data/db_access.php";
-$que="insert into ".$homename."_board (title,time,writer,content,upper) values('".$_POST['title']."',".time().",'".$_POST['writer']."','".htmlentities($_POST['content'],ENT_QUOTES)."',".$_POST['upper'].")";
+$que="insert into ".$homename."_board (title,time,writer,content,upper) values('".htmlentities($_POST['title'],ENT_QUOTES)."',".time().",'".$_POST['writer']."','".htmlentities($_POST['content'],ENT_QUOTES)."',".$_POST['upper'].")";
 mysqli_query($connect,$que);
 echo "<meta http-equiv=\"refresh\" content=\"0;url=.\">\n";
 ?>
