eb1cd4a81117060cd63d05468ea2fcaad19144a2
Jaeuk Ju <jujaeuk@gmail.com>
2019-01-03 10:49:40 +0900

    Update modify_ok.php

--- modify_ok.php
+++ modify_ok.php
@@ -1,7 +1,7 @@
 <?
 include "lib.php";
 include "data/db_access.php";
-$que="update ".$homename."_board set title='".$_POST['title']."', content='".htmlentities($_POST['content'],ENT_QUOTES)."', upper=".$_POST['upper']." where no=".$_POST['no'];
+$que="update ".$homename."_board set title='".htmlentities($_POST['title'],ENT_QUOTES)."', content='".htmlentities($_POST['content'],ENT_QUOTES)."', upper=".$_POST['upper']." where no=".$_POST['no'];
 mysqli_query($connect,$que);
 echo "<meta http-equiv=\"refresh\" content=\"0;url=read.php?no=".$_POST['no']."\">\n";
 ?>
