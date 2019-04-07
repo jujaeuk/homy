bb077cf0cb9fb10e548b38a8b5bad86880bd1cb4
Jaeuk Ju <jujaeuk@gmail.com>
2019-04-07 12:12:34 +0900

    Update and rename base.php to board_reset.php

--- board_reset.php
+++ board_reset.php
@@ -3,7 +3,7 @@ include "lib.php";
 include "data/db_access.php";
 include "head.php";
 echo "<article>\n";
-echo "this is a homy base file.\n";
+echo "board will be deleted. are you sure? <a href=board_reset_ok.php>yes</a> <a href=.>no</a>\n";
 echo "</article>\n";
 ?>
 </div></div>
