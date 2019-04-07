e9187913f1c39019b8d49452178a80da8c754d2f
Jaeuk Ju <jujaeuk@gmail.com>
2019-04-07 12:14:33 +0900

    Create board_reset_ok.php

---
+++ board_reset_ok.php
@@ -0,0 +1,19 @@
+<?
+include "lib.php";
+include "data/db_access.php";
+
+$que="drop table ".$homename."_board";
+mysqli_query($connect,$que);
+
+$que="create table ".$_POST['homename']."_board(
+  no int not null auto_increment,
+  unique(no),
+  primary key(no),
+  title char(128),
+  time int,
+  writer char(32),
+  content text,
+  upper int)";
+mysqli_query($connect,$que);
+?>
+<meta http-equiv="refresh" content="0;url=.">
