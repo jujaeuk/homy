db4a64dc123af5675eb099dd8d16cbdbe87b0ebf
Jaeuk Ju <jujaeuk@gmail.com>
2019-05-26 15:08:40 +0900

    Update board_backup.php

--- board_backup.php
+++ board_backup.php
@@ -9,19 +9,19 @@ $que="select * from ".$homename."_board order by time";
 $result=mysqli_query($connect,$que);
 $i=0;
 while(@$check=mysqli_fetch_object($result)){
-  fwrite($fp,"no. ".$check->no." ".$check->title." (".$check->writer.", ".date("Y-m-d H:i",$check->time).")\n");
+  fwrite($fp,"no. ".$check->no." ".html_entity_decode($check->title)." (".$check->writer.", ".date("Y-m-d H:i",$check->time).")\n");
   fwrite($fp,html_entity_decode($check->content)."\n");
   if($check->upper!=0){
     $que="select * from ".$homename."_board where no=$check->upper";
     $check_upper=mysqli_fetch_object(mysqli_query($connect,$que));
-    fwrite($fp,"upper: no.".$check_upper->no." ".$check_upper->title."\n");
+    fwrite($fp,"upper: no.".$check_upper->no." ".html_entity_decode($check_upper->title)."\n");
   }
   $que="select * from ".$homename."_board where upper=".$check->no;
   $result_lower=mysqli_query($connect,$que);
   if(mysqli_num_rows($result_lower)>0){
     fwrite($fp,"lower:\n");
     while($check_lower=mysqli_fetch_object($result_lower)){
-      fwrite($fp,"no .".$check_lower->no." ".$check_lower->title."\n");
+      fwrite($fp,"no .".$check_lower->no." ".html_entity_decode($check_lower->title)."\n");
     }
   }
   fwrite($fp,"\n");
@@ -32,19 +32,19 @@ function subcontents($fp,$connect,$homename,$upper){
   $result=mysqli_query($connect,$que);
   if(mysqli_num_rows($result)){
     while(@$check=mysqli_fetch_object($result)){
-      fwrite($fp,"no. ".$check->no." ".$check->title." (".$check->writer.", ".date("Y-m-d H:i",$check->time).")\n");
+      fwrite($fp,"no. ".$check->no." ".html_entity_decode($check->title)." (".$check->writer.", ".date("Y-m-d H:i",$check->time).")\n");
       fwrite($fp,html_entity_decode($check->content)."\n");
       if($check->upper!=0){
         $que="select * from ".$homename."_board where no=$check->upper";
         $check_upper=mysqli_fetch_object(mysqli_query($connect,$que));
-        fwrite($fp,"upper: no.".$check_upper->no." ".$check_upper->title."\n");
+        fwrite($fp,"upper: no.".$check_upper->no." ".html_entity_decode($check_upper->title)."\n");
       }
       $que="select * from ".$homename."_board where upper=".$check->no;
       $result_lower=mysqli_query($connect,$que);
       if(mysqli_num_rows($result_lower)>0){
         fwrite($fp,"lower:\n");
         while($check_lower=mysqli_fetch_object($result_lower)){
-          fwrite($fp,"no .".$check_lower->no." ".$check_lower->title."\n");
+          fwrite($fp,"no .".$check_lower->no." ".html_entity_decode($check_lower->title)."\n");
         }
       }
       fwrite($fp,"\n");
