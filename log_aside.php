9f47e92a5f6a72028ff3ad8317ba3dc0722bdb04
Jaeuk Ju <jujaeuk@gmail.com>
2020-01-19 00:47:56 +0900

    Delete log_aside.php

--- log_aside.php
+++
@@ -1,7 +0,0 @@
-<?
-  echo "</div>\n";
-  echo "<div id=aside>\n";
-  echo "<ul><li><a href=log_backup.php>backup</a></li>\n".
-    "<li><a href=log_category_edit.php>edit category</a></li>\n".
-    "<li><a href=log_stat.php>stat</a></li></ul>\n";
-?>
