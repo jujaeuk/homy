2b0e1e59635839f4dc9a75295106ffd2b4646cc9
Jaeuk Ju <jujaeuk@gmail.com>
2020-02-05 09:59:24 +0900

    Update delete.php

--- delete.php
+++ delete.php
@@ -8,7 +8,7 @@ if(mysqli_num_rows($result)){
   include "login.php";
   echo "<div id=container";
   if(!is_mobile()) echo " style=\"display: flex;\"";
-  echo "><div id=main>
+  echo "><div id=main>\n";
   echo "ERROR: related posts exist\n";
   echo "<a href=read.php?no=".$_GET['no'].">back</a>\n";
   echo "</div></div></body></html>\n";
