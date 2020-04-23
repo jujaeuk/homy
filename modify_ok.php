4906d6b0019cbf026f66469d5ff02809e56841f9
Jaeuk Ju <jujaeuk@gmail.com>
2020-04-23 13:46:54 +0900

    Add files via upload

--- modify_ok.php
+++ modify_ok.php
@@ -1,6 +1,7 @@
 <?
 include "lib.php";
 include "data/db_access.php";
+if(!$_POST['title']) $_POST['title']="무제";
 $que="update ".$homename."_board set title='".htmlentities($_POST['title'],ENT_QUOTES)."', content='".htmlentities($_POST['content'],ENT_QUOTES)."', upper=".$_POST['upper'].", order_lower='".$_POST['order_lower']."' where no=".$_POST['no'];
 mysqli_query($connect,$que);
 echo "<meta http-equiv=\"refresh\" content=\"0;url=read.php?no=".$_POST['no']."\">\n";
--- write_ok.php
+++ write_ok.php
@@ -1,6 +1,7 @@
 <?
 include "lib.php";
 include "data/db_access.php";
+if(!$_POST['title']) $_POST['title']="무제";
 $que="insert into ".$homename."_board (title,time,writer,content,upper,order_lower) values('".htmlentities($_POST['title'],ENT_QUOTES)."',".time().",'".$_POST['writer']."','".htmlentities($_POST['content'],ENT_QUOTES)."',".$_POST['upper'].",'".$_POST['order_lower']."')";
 mysqli_query($connect,$que);
 echo "<meta http-equiv=\"refresh\" content=\"0;url=board.php\">\n";
