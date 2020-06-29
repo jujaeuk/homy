88c59e8033b7738286d295b09de2a3cb6dc5574a
Jaeuk Ju <jujaeuk@gmail.com>
2020-06-29 14:48:08 +0900

    Add files via upload

--- write_ok.php
+++ write_ok.php
@@ -4,5 +4,5 @@ include "data/db_access.php";
 if(!$_POST['title']) $_POST['title']="무제";
 $que="insert into ".$homename."_board (title,time,writer,content,upper,order_lower) values('".htmlentities($_POST['title'],ENT_QUOTES)."',".time().",'".$_POST['writer']."','".htmlentities($_POST['content'],ENT_QUOTES)."',".$_POST['upper'].",'".$_POST['order_lower']."')";
 mysqli_query($connect,$que);
-echo "<meta http-equiv=\"refresh\" content=\"0;url=board.php\">\n";
+echo "<meta http-equiv=\"refresh\" content=\"0;url=read.php?no=".$_POST['upper']."\">\n";
 ?>
