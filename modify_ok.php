f970fd319dd551e1414bdb1b26000a0ec52aa2f2
Jaeuk Ju <jujaeuk@gmail.com>
2020-08-30 15:28:36 +0900

    Add files via upload

--- modify_ok.php
+++ modify_ok.php
@@ -15,7 +15,7 @@ if(isset($_FILES)){
 }
  
 if(!$_POST['title']) $_POST['title']="무제";
-$que="update ".$homename."_board set title='".htmlentities($_POST['title'],ENT_QUOTES)."', content='".htmlentities($_POST['content'],ENT_QUOTES)."', upper=".$_POST['upper'].", order_lower='".$_POST['order_lower']."', timeline=".$_POST['timeline']." where no=".$_POST['no'];
+$que="update ".$homename."_board set title='".htmlentities($_POST['title'],ENT_QUOTES)."', content='".htmlentities($_POST['content'],ENT_QUOTES)."', upper=".$_POST['upper'].", order_lower='".$_POST['order_lower']."'  where no=".$_POST['no'];
 
 mysqli_query($connect,$que);
 echo "<meta http-equiv=\"refresh\" content=\"0;url=read.php?no=".$_POST['no']."\">\n";
