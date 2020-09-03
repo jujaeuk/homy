ebcd0bb6a8b29d6f0ed02d9c349a8efa59050e2f
Jaeuk Ju <jujaeuk@gmail.com>
2020-09-03 21:19:43 +0900

    Delete close.php

--- close.php
+++
@@ -1,7 +0,0 @@
-<?
-include "lib.php";
-include "data/db_access.php";
-$que="update ".$homename."_board set showlist=0 where no=".$_GET['no'];
-mysqli_query($connect,$que);
-?>
-<meta http-equiv="refresh" content="0;url=index.html?upper=<?echo $_GET['upper'];?>">
