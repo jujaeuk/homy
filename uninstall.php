60ac376932c2c04d6ecd0a8e22c757fd764b9c64
Jaeuk Ju <jujaeuk@gmail.com>
2020-09-04 02:13:00 +0900

    Add files via upload

--- uninstall.php
+++ uninstall.php
@@ -1,14 +1,20 @@
 <?
 include "lib.php";
 include "head.php";
+$que="select * from ".$homename."_users where name='".$_COOKIE['user']."'";
+@$check=mysqli_fetch_object(mysqli_query($connect,$que));
+echo "<div id=container><div id=main>\n";
+if($check->no==1){
+  echo "<form method=post action=uninstall_ok.php>\n";
+  echo "<table>\n";
+  echo "<tr><td><input type=radio name=uninstall value='table'>table\n";
+  echo "<input type=radio name=uninstall value='skin' checked>skin</td><tr>\n";
+  echo "<tr><td align=center><input type=submit value=uninstall></td><tr>\n";
+  echo "</table>\n";
+  echo "</form>\n";
+}
+else echo "<article>you don't have the right</article>\n";
 ?>
-<form method=post action=uninstall_ok.php>
-<table>
-<tr><td><input type=radio name=uninstall value='table'>table
-<input type=radio name=uninstall value='skin' checked>skin</td><tr>
-<tr><td align=center><input type=submit value=uninstall></td><tr>
-</table>
-</form> 
-</div></div>
+</div><div id=menu></div></div>
 </body>
 </html>
