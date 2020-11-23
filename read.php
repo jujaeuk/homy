bc1a3d4ebd0af6f4214da997b5d331f12080ccf6
Jaeuk Ju <jujaeuk@gmail.com>
2020-11-24 02:23:41 +0900

    Update read.php

--- read.php
+++ read.php
@@ -10,8 +10,9 @@ else $que="select * from ".$homename."_board order by time desc limit 1";
 @$check=mysqli_fetch_object(mysqli_query($connect,$que));
 echo "<article>\n";
 echo "<h3>".$check->title."</h3>\n";
-echo "<p>".date("Y-m-d H:i",$check->time)." <a href=write.php?upper=$check->no>sub</a> ".
-  "<a href=modify.php?no=$check->no>mod</a> <a href=delete.php?no=$check->no&upper=$check->upper>del</a></p>\n";
+echo "<p>by ".$check->writer." ".date("Y-m-d H:i",$check->time)." <a href=write.php?upper=$check->no>아랫글쓰기</a>";
+if($_COOKEI['user']==$check->writer) echo " <a href=modify.php?no=$check->no>수정</a> <a href=delete.php?no=$check->no&upper=$check->upper>삭제</a>";
+echo "</p>\n";
 echo "<p>".nl2br($check->content)."</p>\n";
 if($check->file) echo "<p>file: <a href=files/".$check->file.">".$check->file."</a></p>\n";
 echo "</article>\n";
