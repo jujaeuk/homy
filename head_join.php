f45473b608e0a7b54a00bd3fb888f759d24b9470
Jaeuk Ju <jujaeuk@gmail.com>
2020-11-25 13:58:06 +0900

    Update head_join.php

--- head_join.php
+++ head_join.php
@@ -8,25 +8,4 @@
 </head>
 <body>
 <header><div id=head><a href=.><img src=logo.png align=left></a>
-<?
-echo "<form method=post action=login_ok.php>\n";
-if(is_mobile()){
-  if(get_platform()=="Android"){
-    echo "user <input type=text name=username class=login size=10>\n";
-    echo "password <input type=password name=password class=login size=10>\n";
-    echo "<input type=submit value=login>\n";
-  }
-  else{
-    echo "<table><tr><td>user</td><td><input type=text name=username class=login></td></tr>\n";
-    echo "<tr><td>password</td><td><input type=password name=password class=login></td></tr>\n";
-    echo "<tr><td colspan=2 align=center><input type=submit value=login><td></tr></table>\n";
-  } 
-}
-else{
-  echo "user <input type=text name=username class=login>\n";
-  echo "password <input type=password name=password class=login>\n";
-  echo "<input type=submit value=login>\n";
-}
-echo "</form>\n";
-?>
 </div></header>
