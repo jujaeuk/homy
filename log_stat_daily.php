492c7226115186201e685895902fa57e3b8d42c5
Jaeuk Ju <jujaeuk@gmail.com>
2020-12-07 11:35:14 +0900

    Add files via upload

--- log_stat_daily.php
+++ log_stat_daily.php
@@ -18,7 +18,7 @@ function stat_day($day,$homename,$connect,$cate_list){
   while($check=mysqli_fetch_object($result)){
     $islist=0;
     $time=round(($check->end-$check->start)/60)-$check->loss;
-    if(is_array($cate_list)||is_object($cate_list)){
+    if(isset($cate_list)){
       for($i=0;$i<sizeof($cate_list);$i++){
         if($cate_list[$i]==$check->category){
           $islist=1;
@@ -27,14 +27,15 @@ function stat_day($day,$homename,$connect,$cate_list){
       }
     }
     if($islist==0){
-      if(is_array($cate_list)||is_object($cate_list)) $new=sizeof($cate_list);
+      if(isset($cate_list)) $new=sizeof($cate_list);
+      else $new=0;
       $cate_list[$new]=$check->category;
       $time_list[$new]=$time; 
     }
   }
   echo "<table>\n";
   echo "<tr><td colspan=2>".date("m/d", $the_day)."</td></tr>\n";
-  if(is_array($cate_list)||is_object($cate_list)){
+  if(isset($cate_list)){
     for($i=0;$i<sizeof($cate_list);$i++){
       if($cate_list[$i]!="기록") echo "<tr><td>".$cate_list[$i]."</td><td align=right>".$time_list[$i]."</td></tr>\n";
     }
