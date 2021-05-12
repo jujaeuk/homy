e53eb00923ef5ff87b45d08c875fd29394b221a3
Jaeuk Ju <jujaeuk@gmail.com>
2021-05-12 12:23:35 +0900

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -6,13 +6,7 @@ $que="select * from ".$homename."_users where name='".$_COOKIE['user']."'";
 @$check=mysqli_fetch_object(mysqli_query($connect,$que));
 echo "<div id=container><div id=main>\n";
 if($check->no==1){
-  echo "<form method=post action=uninstall_ok.php>\n";
-  echo "<table>\n";
-  echo "<tr><td><input type=radio name=uninstall value='table'>table\n";
-  echo "<input type=radio name=uninstall value='skin' checked>skin</td><tr>\n";
-  echo "<tr><td align=center><input type=submit value=uninstall></td><tr>\n";
-  echo "</table>\n";
-  echo "</form>\n";
+  echo "are you sure? <a href=uninstall_ok.hp>yes</a> <a href=.>no</a>\n";
 }
 else{
   echo "<article>you don't have the right</article>\n";
