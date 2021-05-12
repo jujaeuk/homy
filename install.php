7b1d0ccaf532e23e98aea8154d4ade2daba8634d
Jaeuk Ju <jujaeuk@gmail.com>
2021-05-12 12:19:28 +0900

    Update install.php

--- install.php
+++ install.php
@@ -6,8 +6,6 @@ include "head_join.php";
 <h2>install</h2>
 <form method=post action=install_ok.php>
 <table>
-<tr><td colspan=2><input type=radio name=install value='table'>full(table)
-<input type=radio name=install value='skin' checked>skin only</td><tr>
 <tr><td>home name</td><td><input type=text name=homename></td></tr>
 <tr><td>host</td><td><input type=text name=host></td></tr>
 <tr><td>user</td><td><input type=text name=user></td></tr>
