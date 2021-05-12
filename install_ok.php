88e5e0cb4a87700a309d4164d7a307329e51b7fa
Jaeuk Ju <jujaeuk@gmail.com>
2021-05-12 12:20:44 +0900

    Update install_ok.php

--- install_ok.php
+++ install_ok.php
@@ -21,52 +21,52 @@ else{
          "\";\n\$db=\"".$_POST['db']."\";\n");
   fwrite($fp, "\$connect=mysqli_connect(\$host,\$user,\$password,\$db) or die(\"DB connection error\");\n?>\n");
   fclose($fp);
-  if($_POST['install']=="table"){
-    $que="create table ".$_POST['homename']."_users(
-      no int not null auto_increment,
-      unique(no),
-      primary key(no),
-      name char(32),
-      password char(32))";
-    mysqli_query($connect,$que);
-    $que="create table ".$_POST['homename']."_board(
-      no int not null auto_increment,
-      unique(no),
-      primary key(no),
-      title char(128),
-      time int,
-      writer char(32),
-      content text,
-      upper int,
-      order_lower char(16) default 'time',
-      subno int default 1)";
-    mysqli_query($connect,$que);
-    $que="create table ".$_POST['homename']."_ref(
-      no int not null auto_increment,
-      primary key(no),
-      origin int,
-      ref int)";
-    mysqli_query($connect,$que);
-    $que="create table ".$_POST['homename']."_log(
-      no int not null auto_increment,
-      unique(no),
-      primary key(no),
-      start int,
-      end int,
-      loss int default 0,
-      category char(32),
-      content char(128),
-      due int,
-      up int)";
-    mysqli_query($connect,$que);
-    $que="create table ".$_POST['homename']."_files(
-      no int not null auto_increment,
-      unique(no),
-      primary key(no),
-      boardno int,
-      filename char(128))";
-    mysqli_query($connect,$que);
-  }
+
+  $que="create table ".$_POST['homename']."_users(
+    no int not null auto_increment,
+    unique(no),
+    primary key(no),
+    name char(32),
+    password char(32))";
+  mysqli_query($connect,$que);
+  $que="create table ".$_POST['homename']."_board(
+    no int not null auto_increment,
+    unique(no),
+    primary key(no),
+    title char(128),
+    time int,
+    writer char(32),
+    content text,
+    upper int,
+    order_lower char(16) default 'time',
+    subno int default 1)";
+  mysqli_query($connect,$que);
+  $que="create table ".$_POST['homename']."_ref(
+    no int not null auto_increment,
+    primary key(no),
+    origin int,
+    ref int)";
+  mysqli_query($connect,$que);
+  $que="create table ".$_POST['homename']."_log(
+    no int not null auto_increment,
+    unique(no),
+    primary key(no),
+    start int,
+    end int,
+    loss int default 0,
+    category char(32),
+    content char(128),
+    due int,
+    up int)";
+  mysqli_query($connect,$que);
+  $que="create table ".$_POST['homename']."_files(
+    no int not null auto_increment,
+    unique(no),
+    primary key(no),
+    boardno int,
+    filename char(128))";
+  mysqli_query($connect,$que);
+
   include "data/db_access.php";
   echo "home ".$_POST['homename']." created<br><a href=.>home</a>\n";
 }
